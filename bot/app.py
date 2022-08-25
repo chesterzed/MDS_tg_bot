1bb7697f6663a500ec616d215be1b4e2f481b832
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-25 21:16:07 +0300

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -2,6 +2,7 @@ from aiogram import executor
 
 import hook as h
 import cherrypy
+import logging
 
 from loader import dp, bot
 import middlewares, filters, handlers
@@ -18,27 +19,52 @@ async def on_startup(dispatcher):
     # Уведомляет про запуск
     await on_startup_notify(dispatcher)
 
+async def on_startup_h(dp):
+    await bot.set_webhook(h.WEBHOOK_URL_BASE)
+    # insert code here to run it after start
 
-if __name__ == '__main__':
+
+async def on_shutdown_h(dp):
+    logging.warning('Shutting down..')
+
+    # Remove webhook (not acceptable in some cases)
     await bot.delete_webhook()
 
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
+
+if __name__ == '__main__':
+    # bot.delete_webhook()
+
     # Ставим заново вебхук
-    await bot.set_webhook(url=h.WEBHOOK_URL_BASE + h.WEBHOOK_URL_PATH,
-                    certificate=open(h.WEBHOOK_SSL_CERT, 'r'))
+    # bot.set_webhook(url=h.WEBHOOK_URL_BASE + h.WEBHOOK_URL_PATH,
+    #                 certificate=open(h.WEBHOOK_SSL_CERT, 'r'))
 
-    # Указываем настройки сервера CherryPy
-    cherrypy.config.update({
-        'server.socket_host': h.WEBHOOK_LISTEN,
-        'server.socket_port': h.WEBHOOK_PORT,
-        'server.ssl_module': 'builtin',
-        'server.ssl_certificate': h.WEBHOOK_SSL_CERT,
-        'server.ssl_private_key': h.WEBHOOK_SSL_PRIV
-    })
+    # # Указываем настройки сервера CherryPy
+    # cherrypy.config.update({
+    #     'server.socket_host': h.WEBHOOK_LISTEN,
+    #     'server.socket_port': h.WEBHOOK_PORT,
+    #     'server.ssl_module': 'builtin',
+    #     'server.ssl_certificate': h.WEBHOOK_SSL_CERT,
+    #     'server.ssl_private_key': h.WEBHOOK_SSL_PRIV
+    # })
+    # 
+    # # Собственно, запуск!
+    # cherrypy.quickstart(h.WebhookServer(), h.WEBHOOK_URL_PATH, {'/': {}})
 
-    # Собственно, запуск!
-    cherrypy.quickstart(h.WebhookServer(), h.WEBHOOK_URL_PATH, {'/': {}})
+    executor.set_webhook(
+        dispatcher=dp,
+        webhook_path=h.WEBHOOK_URL_PATH,
+        skip_updates=True,
+        on_startup=on_startup_h,
+        on_shutdown=on_shutdown_h,
+        check_ip=False,
 
-
+    )
     executor.start_polling(dp, on_startup=on_startup)
     
     while True:
