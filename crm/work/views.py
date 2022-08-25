ea627d4d0c37f8a9c160baf803085fa5d4c85d15
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-25 23:02:18 +0300

    test2

--- crm/work/views.py
+++ crm/work/views.py
@@ -313,6 +313,7 @@ def add_mailing(req, id=None):
     if req.method == 'POST':
         data = req.POST.dict()
         print(str(data) + "test")
+        print(str(req.POST) + "test1")
 
         if 'delete' in data and id:
             mailing = Mailing.objects.get(id=id)
