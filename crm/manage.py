4a0119d55ff8e4b023ef62d6745d076736a80945
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-27 02:36:44 +0300

    Update manage.py

--- crm/manage.py
+++ crm/manage.py
@@ -17,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    checker.user_leave_checker()
 
 
 if __name__ == '__main__':
