9ad26919f534eacd532640135a9bc5c28aacf3f0
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-26 23:59:23 +0300

    Update views.py

--- crm/work/views.py
+++ crm/work/views.py
@@ -342,9 +342,9 @@ def add_mailing(req, id=None):
         mailing.desc = data['desc']
 
         file = req.FILES['photo']
-        file_name = default_storage.save(f'{mailing.id}.{str(file.name).split(".")[-1]}', file)
+        file_name = default_storage.save(f'{str(file.name).split(".")[-1]}', file)
 
-        mailing.photo = f'../crm/media/{file_name}'
+        mailing.photo = f'./crm/media/{file_name}'
         # mailing.photo = f'../crm/media/{mailing.id}.{str(file.name).split(".")[-1]}'
 
         mailing.save()
