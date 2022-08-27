4de555a55124fb71354e3aec5e9b9fb28d294b77
root <root@703767-cq91720.tmweb.ru>
2022-08-27 03:43:44 +0300

    ch

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 bot=$(ps -ef | grep bot/app.py | awk '{print $2}')
 crm=$(ps -ef | grep crm/manage.py | awk '{print $2}')
 echo $(ps -ef | grep bot/app.py | awk '{print $2}')
-kill $bot
-kill $crm
+kill -9 $bot
+kill -9 $crm
 
 git pull
 
