e496426336602d8854578adbbeefcc128544c5b5
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-29 23:40:06 +0300

    Update views.py

--- crm/work/views.py
+++ crm/work/views.py
@@ -41,7 +41,7 @@ def main(req, page=None):
         elif page == 'statistic':
             """Страница управления контентом"""
             stat = Statistic.objects.all()
-            return render(req, 'work/main.html', {'statistic': stat, 'page': page})
+            return render(req, 'work/main.html', {'page': page, 'statistic': stat})
 
         else:
             users = User.objects.all()
