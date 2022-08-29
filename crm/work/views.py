cdb288e86d13d6f6b3a5253761d62eec0b693a6d
____MAX____ <96175787+qmaximp@users.noreply.github.com>
2022-08-30 01:18:24 +0300

    Update views.py

--- crm/work/views.py
+++ crm/work/views.py
@@ -141,9 +141,8 @@ def user_page(req, id=None):
         if req.method == 'POST':
             data = req.POST.dict()
             user.name = data['name']
-            user.about = data['about']
-            
             user.photo = f"../crm/media/{data['phone']}.{str(file.name).split('.')[-1]}"
+            user.about = data['about']
             user.about_1 = data['about_1']
             user.about_2 = data['about_2']
             user.about_3 = data['about_3']
