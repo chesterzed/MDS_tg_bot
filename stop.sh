c98ef4d2a410c52a4825fd2b3213220c3b0b37bb
root <root@703767-cq91720.tmweb.ru>
2022-08-30 20:44:32 +0300

    shell

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 bot=$(ps -ef | grep bot/app.py | awk '{print $2}')
 crm=$(ps -ef | grep crm/manage.py | awk '{print $2}')
-che=$(ps -ef | grep bot/app.py | awk '{print $2}')
+che=$(ps -ef | grep bot/checker.py | awk '{print $2}')
 kill -9 $bot
 kill -9 $crm
 kill -9 $che
