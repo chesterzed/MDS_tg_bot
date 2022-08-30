a5da44ee67c7cce065447e0da18b34eb00b19dca
Ratibor Vanin <chester.1.80@mail.ru>
2022-08-31 02:19:56 +0300

    Update anketa.py

--- bot/handlers/users/anketa.py
+++ bot/handlers/users/anketa.py
@@ -8,7 +8,7 @@ from states import Anketa
 from sql import User
 from keyboards.default import main_kb
 
-from checker import update_stats, get_table, db_connect
+from root.club2.crm.checker import update_stats, get_table, db_connect
 
 
 @dp.message_handler(content_types=['contact'], state=Anketa.name)
