412d6b899a2681dd32ab3a049b25e0c5a48c099a
Ratibor Vanin <chester.1.80@mail.ru>
2022-09-10 16:07:25 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 bot=$(ps -ef | grep bot/app.py | awk '{print $2}')
+b_starter=$(ps -ef | grep start_bot.sh | awk '{print $2}')
 crm=$(ps -ef | grep crm/manage.py | awk '{print $2}')
 che=$(ps -ef | grep crm/checker.py | awk '{print $2}')
 kill -9 $bot
 kill -9 $crm
 kill -9 $che
+kill -9 $b_starter
