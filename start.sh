f8a840767cddf9e50eb8b55c8a0920cee8bc292b
Ratibor Vanin <chester.1.80@mail.ru>
2022-09-10 16:06:25 +0300

    check

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 bot=$(ps -ef | grep bot/app.py | awk '{print $2}')
+b_starter=$(ps -ef | grep start_bot.sh | awk '{print $2}')
 crm=$(ps -ef | grep crm/manage.py | awk '{print $2}')
 checker=$(ps -ef | grep crm/checker.py | awk '{print $2}')
 kill -9 $bot
 kill -9 $crm
 kill -9 $checker
+kill -9 $b_starter
 
 git pull
 
 python3 crm/manage.py runserver 109.68.213.180:1000 &
 python3 crm/checker.py &
-sh start_bot.sh
+sh start_bot.sh &
--- start_bot.sh
+++ start_bot.sh
@@ -2,5 +2,7 @@ while true
 do
   bot=$(ps -ef | grep bot/app.py | awk '{print $2}')
   kill -9 $bot
-  sleep 86400
+  echo $bot
+  python3 bot/app.py &
+  sleep 5
 done
